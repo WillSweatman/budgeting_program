303e38da393c4fecef773568665f46292932464e
Will Sweatman <will.sweatman@quickrelease.co.uk>
2023-05-09 15:26:12 +0100

    added a window and trying staged commit

--- main_window.py
+++ main_window.py
@@ -1,1 +1,13 @@
-print("Hello World")
+import tkinter as tk
+
+# Create a new window
+window = tk.Tk()
+
+# Set the window title
+window.title("Budgeting App")
+
+# Set the window size
+window.geometry("400x400")
+
+# Start the window event loop
+window.mainloop()
