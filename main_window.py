9c1510a2e0943202f09f9c566ca3873740b53305
Will Sweatman <will.sweatman@quickrelease.co.uk>
2023-05-09 16:21:52 +0100

    added salary entry and all taxes

--- main_window.py
+++ main_window.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import numpy as np
 
 class MyWindow(tk.Tk):
     def __init__(self):
@@ -7,31 +8,47 @@ class MyWindow(tk.Tk):
         self.title("Budgeting App")
         self.geometry("400x400")
         
-        self.label = tk.Label(self, text="Hello, world!")
-        self.label.pack()
+        self.init_salary = 0
+        self.salary_entry = tk.Entry(self)
+        self.salary_entry.pack()
+
+        self.student_loan_check_var = tk.BooleanVar()
+        self.student_loan_check = tk.Checkbutton(self, text="Student Loan", variable=self.student_loan_check_var)
+        self.student_loan_check.pack()
         
-        self.button = tk.Button(self, text="Click me!", command=self.on_click)
+        self.button = tk.Button(self, text="Calculate", command=self.on_click)
         self.button.pack()
+
+        self.label = tk.Label(self, text="")
+        self.label.pack()
+
         
     def on_click(self):
-        self.label.config(text=self.allTaxes())
+        self.init_salary = float(self.salary_entry.get())
+        
+        self.label.config(text="Monthly salary = "+str(self.allTaxes()))
 
     def allTaxes(self):
-        salary = init_salary = 33000
+        salary = self.init_salary
 
-        # basic income
-        salary -= self.singleTax(init_salary, 12571, 0.20)
+        # basic income bands
+        salary -= self.singleTax(12571, 0.20)
+        salary -= self.singleTax(50271, 0.20) # rolling calc, so 20+20=40% tax
+        salary -= self.singleTax(125140, 0.05) # dito, 20+20+5=45% tax
 
         # national insurance
-        salary -= self.singleTax(init_salary, 1048*12, 0.12)
+        salary -= self.singleTax(1048*12, 0.12)
 
         # student loan
-        salary -= self.singleTax(init_salary, 27295, 0.09)
+        salary -= self.singleTax(27295, 0.09)
 
-        return salary
+        return np.round(salary/12, 2)
 
-    def singleTax(self, init_salary, band, rate):
-        return (init_salary - band) * rate
+    def singleTax(self, band, rate):
+        if self.init_salary < band:
+            return 0
+        
+        return (self.init_salary - band) * rate
 
         
 
