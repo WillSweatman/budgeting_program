771287f3cbe439e866a72e0ada475a2ab5724d52
Will Sweatman <will.sweatman@quickrelease.co.uk>
2023-05-09 15:58:04 +0100

    adding salary and tax to button response

--- main_window.py
+++ main_window.py
@@ -1,13 +1,40 @@
 import tkinter as tk
 
-# Create a new window
-window = tk.Tk()
+class MyWindow(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        
+        self.title("Budgeting App")
+        self.geometry("400x400")
+        
+        self.label = tk.Label(self, text="Hello, world!")
+        self.label.pack()
+        
+        self.button = tk.Button(self, text="Click me!", command=self.on_click)
+        self.button.pack()
+        
+    def on_click(self):
+        self.label.config(text=self.allTaxes())
 
-# Set the window title
-window.title("Budgeting App")
+    def allTaxes(self):
+        salary = init_salary = 33000
 
-# Set the window size
-window.geometry("400x400")
+        # basic income
+        salary -= self.singleTax(init_salary, 12571, 0.20)
 
-# Start the window event loop
-window.mainloop()
+        # national insurance
+        salary -= self.singleTax(init_salary, 1048*12, 0.12)
+
+        # student loan
+        salary -= self.singleTax(init_salary, 27295, 0.09)
+
+        return salary
+
+    def singleTax(self, init_salary, band, rate):
+        return (init_salary - band) * rate
+
+        
+
+if __name__ == "__main__":
+    app = MyWindow()
+    app.mainloop()
