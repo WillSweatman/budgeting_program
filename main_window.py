e01b6d29d793d4c2bc9bded43a0214950f29dd11
Will Sweatman <will.sweatman@quickrelease.co.uk>
2023-05-11 14:09:55 +0100

    moved tab content into separate functions

--- main_window.py
+++ main_window.py
@@ -12,23 +12,24 @@ class MyWindow(tk.Tk):
         super().__init__()
         
         self.title("Budgeting App")
-        width=500
-        self.geometry(str(width)+"x"+str(width))
+        self.width=500
+        self.geometry(str(self.width)+"x"+str(self.width))
 
-        # Set the custom style
+        # Set the custom style (not implemented)
         #custom_style = set_custom_style()
 
         # in the mean time
         self.dark = "#23404b"
         self.dark2 = "#264653"
         self.blue = "#488299"
-        red = "#e76f51"
-        red2 = "#e97c61"
-        amber = "#f4a261"
-        amber2 = "#efb366"
-        green = "#2a9d8f"
-        green2 = "#5aa786"
+        self.red = "#e76f51"
+        self.red2 = "#e97c61"
+        self.amber = "#f4a261"
+        self.amber2 = "#efb366"
+        self.green = "#2a9d8f"
+        self.green2 = "#5aa786"
         
+        # creating notebook format
         self.notebook = ttk.Notebook(self)
         self.notebook.pack(side=tk.TOP, fill='both', expand=True)
         self.tab1 = tk.Frame(self.notebook, bg=self.dark)
@@ -37,118 +38,15 @@ class MyWindow(tk.Tk):
         self.notebook.add(self.tab2, text='Expenses')
         self.tab3 = tk.Frame(self.notebook)
         self.notebook.add(self.tab3, text='Plot')
-        self.tab4 = tk.Frame(self.notebook)
+        self.tab4 = tk.Frame(self.notebook, bg=self.blue)
         self.notebook.add(self.tab4, text='Help')
 
-        """tab1"""
-
-        core = tk.Frame(self.tab1, bg=self.dark2)
-        core.pack()
-
-        self.label_title = tk.Label(core, bg=self.blue, text="Enter Salary Below")
-        self.label_title.pack()
-
-        self.init_salary = 0
-        self.salary_entry = tk.Entry(core, bg=self.blue, fg="white")
-        self.salary_entry.insert(0, "33000")
-        self.salary_entry.pack()
-
-        self.loan_check_var = tk.BooleanVar(value=True)
-        self.loan_check = tk.Checkbutton(core, bg=self.blue, text="Student Loan", variable=self.loan_check_var)
-        self.loan_check.pack()
+        # filling tabs with content
+        self.tab1Content()
+        self.tab2Content()
+        #self.tab3Content()
+        self.tab4Content()
         
-        self.button = tk.Button(core, bg=self.blue, text="Tax me", command=self.onClick)
-        self.button.pack()
-
-        self.label_monthly = tk.Label(core, text="", bg=self.dark2, fg="white")
-        self.label_monthly.pack()
-
-        ratios = tk.Frame(self.tab1, bg=self.dark2)
-        ratios.pack()
-
-        self.label_scrolls = tk.Label(ratios, bg=self.dark2, fg="white", text="Need : Want : Use (Roughly 50:30:20)" + 
-                                      "\nNeed shouldnt breach 50, and dictate the others")
-        self.label_scrolls.grid(row=0)
-
-        ratios_1 = tk.Frame(ratios, width=width/1.5, bg=red2)
-        ratios_1.grid(row=1, sticky="nsew")
-        ratios_2 = tk.Frame(ratios, width=width/1.5, bg=amber2)
-        ratios_2.grid(row=2, sticky="nsew")
-        ratios_3 = tk.Frame(ratios, width=width/1.5, bg=green2)
-        ratios_3.grid(row=3, sticky="nsew")
-
-        self.label_need = tk.Label(ratios_1, text="\nNeed", bg=red, fg="white")
-        self.label_want = tk.Label(ratios_2, text="\nWant", bg=amber, fg="white")
-        self.label_use = tk.Label(ratios_3, text="\nUse \u2007", bg=green, fg="white")
-
-        self.scroll_need = tk.Scale(ratios_1, from_=0, to=100, orient=tk.HORIZONTAL, bg=red,
-                                    command=lambda *args: self.updateScrollbars("need", *args))
-        self.scroll_want = tk.Scale(ratios_2, from_=0, to=100, orient=tk.HORIZONTAL, bg=amber,
-                                    command=lambda *args: self.updateScrollbars("want", *args))
-        self.scroll_use = tk.Scale(ratios_3, from_=0, to=100, orient=tk.HORIZONTAL, bg=green,
-                                    command=lambda *args: self.updateScrollbars("use", *args))
-        
-        self.label_need_val = tk.Label(ratios_1, text="\u2007"*7, bg=red, fg="white")
-        self.label_want_val = tk.Label(ratios_2, text="\u2007"*7, bg=amber, fg="white")
-        self.label_use_val = tk.Label(ratios_3, text="\u2007"*7, bg=green, fg="white")
-
-        self.label_need.grid(row=0, column=0)
-        self.label_want.grid(row=0, column=0)
-        self.label_use.grid(row=0, column=0)
-        self.scroll_need.grid(row=0, column=1)
-        self.scroll_want.grid(row=0, column=1)
-        self.scroll_use.grid(row=0, column=1)
-        self.label_need_val.grid(row=0, column=2)
-        self.label_want_val.grid(row=0, column=2)
-        self.label_use_val.grid(row=0, column=2)
-
-        for r in [ratios_1, ratios_2, ratios_3]:
-            r.grid_rowconfigure(1, weight=1)
-            r.grid_columnconfigure(1, weight=1)
-        
-
-        """tab2"""
-
-        self.expenses_tree = CustomTreeview(self.tab2, width)
-        self.expenses_tree.pack()
-
-        self.expense_input = tk.Frame(self.tab2)
-        self.expense_input.pack()
-
-        self.name_label = tk.Label(self.expense_input, text="Name:")
-        self.value_label = tk.Label(self.expense_input, text="Value (Monthly):")
-        self.colour_label = tk.Label(self.expense_input, text="Colour:")
-        self.name_entry = tk.Entry(self.expense_input)
-        self.value_entry = tk.Entry(self.expense_input)
-        self.colour_entry = tk.Entry(self.expense_input)
-
-        # just for now!!!, saves me entering it in every time
-        self.name_entry.insert("0", "train")
-        self.value_entry.insert("0", "130")
-        self.colour_entry.insert("0", "#e9c46a")
-
-        self.name_label.grid(row=0, column=0)
-        self.value_label.grid(row=0, column=1)
-        self.colour_label.grid(row=0, column=2)
-        self.name_entry.grid(row=1, column=0)
-        self.value_entry.grid(row=1, column=1)
-        self.colour_entry.grid(row=1, column=2)
-
-        self.input_button = tk.Button(self.expense_input, text="Input", command=self.newExpense)
-        self.input_button.grid(row=2, column=0, columnspan=3)
-
-        """tab4"""
-        self.help_label = tk.Label(self.tab4)
-        self.help_label.pack(side="left", anchor="nw")
-
-        # ive opted to have it as a .txt file and open it so that
-        # its easier to edit and it can be accessed without running
-        # the program every time
-        path = os.path.dirname(__file__) + "\help.txt"
-        with open(path, "r") as f:
-            text = f.read()
-
-        self.help_label.config(text=text, justify="left")
         
     def onClick(self):
         self.init_salary = float(self.salary_entry.get())
@@ -218,6 +116,122 @@ class MyWindow(tk.Tk):
     def newExpense(self):
         new_expense = [self.name_entry.get(), self.value_entry.get(), self.colour_entry.get()]
         self.expenses_tree.addItem(new_expense)
+
+    def tab1Content(self):
+        self.core = tk.Frame(self.tab1, bg=self.dark2)
+        self.core.pack()
+
+        self.label_title = tk.Label(self.core, bg=self.blue, text="Enter Salary Below")
+        self.label_title.pack()
+
+        self.init_salary = 0
+        self.salary_entry = tk.Entry(self.core, bg=self.blue, fg="white")
+        self.salary_entry.insert(0, "33000")
+        self.salary_entry.pack()
+
+        self.loan_check_var = tk.BooleanVar(value=True)
+        self.loan_check = tk.Checkbutton(self.core, bg=self.blue, text="Student Loan", variable=self.loan_check_var)
+        self.loan_check.pack()
+        
+        self.button = tk.Button(self.core, bg=self.blue, text="Tax me", command=self.onClick)
+        self.button.pack()
+
+        self.label_monthly = tk.Label(self.core, text="", bg=self.dark2, fg="white")
+        self.label_monthly.pack()
+
+        ratios = tk.Frame(self.tab1, bg=self.dark2)
+        ratios.pack()
+
+        self.label_scrolls = tk.Label(ratios, bg=self.dark2, fg="white", text="Need : Want : Use (Roughly 50:30:20)" + 
+                                      "\nNeed shouldnt breach 50, and dictate the others")
+        self.label_scrolls.grid(row=0)
+
+        ratios_1 = tk.Frame(ratios, width=self.width/1.5, bg=self.red2)
+        ratios_1.grid(row=1, sticky="nsew")
+        ratios_2 = tk.Frame(ratios, width=self.width/1.5, bg=self.amber2)
+        ratios_2.grid(row=2, sticky="nsew")
+        ratios_3 = tk.Frame(ratios, width=self.width/1.5, bg=self.green2)
+        ratios_3.grid(row=3, sticky="nsew")
+
+        self.label_need = tk.Label(ratios_1, text="\nNeed", bg=self.red, fg="white")
+        self.label_want = tk.Label(ratios_2, text="\nWant", bg=self.amber, fg="white")
+        self.label_use = tk.Label(ratios_3, text="\nUse \u2007", bg=self.green, fg="white")
+
+        self.scroll_need = tk.Scale(ratios_1, from_=0, to=100, orient=tk.HORIZONTAL, bg=self.red,
+                                    command=lambda *args: self.updateScrollbars("need", *args))
+        self.scroll_want = tk.Scale(ratios_2, from_=0, to=100, orient=tk.HORIZONTAL, bg=self.amber,
+                                    command=lambda *args: self.updateScrollbars("want", *args))
+        self.scroll_use = tk.Scale(ratios_3, from_=0, to=100, orient=tk.HORIZONTAL, bg=self.green,
+                                    command=lambda *args: self.updateScrollbars("use", *args))
+        
+        self.label_need_val = tk.Label(ratios_1, text="\u2007"*7, bg=self.red, fg="white")
+        self.label_want_val = tk.Label(ratios_2, text="\u2007"*7, bg=self.amber, fg="white")
+        self.label_use_val = tk.Label(ratios_3, text="\u2007"*7, bg=self.green, fg="white")
+
+        self.label_need.grid(row=0, column=0)
+        self.label_want.grid(row=0, column=0)
+        self.label_use.grid(row=0, column=0)
+        self.scroll_need.grid(row=0, column=1)
+        self.scroll_want.grid(row=0, column=1)
+        self.scroll_use.grid(row=0, column=1)
+        self.label_need_val.grid(row=0, column=2)
+        self.label_want_val.grid(row=0, column=2)
+        self.label_use_val.grid(row=0, column=2)
+
+        for r in [ratios_1, ratios_2, ratios_3]:
+            r.grid_rowconfigure(1, weight=1)
+            r.grid_columnconfigure(1, weight=1)
+
+    def tab2Content(self):
+        self.expenses_tree = CustomTreeview(self.tab2, self.width)
+        self.expenses_tree.pack()
+
+        self.expense_input = tk.Frame(self.tab2)
+        self.expense_input.pack()
+
+        self.name_label = tk.Label(self.expense_input, text="Name:")
+        self.value_label = tk.Label(self.expense_input, text="Value (Monthly):")
+        self.colour_label = tk.Label(self.expense_input, text="Colour:")
+        self.name_entry = tk.Entry(self.expense_input)
+        self.value_entry = tk.Entry(self.expense_input)
+        self.colour_entry = tk.Entry(self.expense_input)
+
+        # just for now!!!, saves me entering it in every time
+        self.name_entry.insert("0", "train")
+        self.value_entry.insert("0", "130")
+        self.colour_entry.insert("0", "#e9c46a")
+
+        self.name_label.grid(row=0, column=0)
+        self.value_label.grid(row=0, column=1)
+        self.colour_label.grid(row=0, column=2)
+        self.name_entry.grid(row=1, column=0)
+        self.value_entry.grid(row=1, column=1)
+        self.colour_entry.grid(row=1, column=2)
+
+        self.input_button = tk.Button(self.expense_input, text="Input", command=self.newExpense)
+        self.input_button.grid(row=2, column=0, columnspan=3)
+
+    def tab3Content(self):
+        raise NotImplementedError("This feature is not implemented yet.")
+
+    def tab4Content(self):
+        self.help_label = tk.Label(self.tab4)
+        self.help_label.pack(side="left", anchor="nw")
+
+        # ive opted to have it as a .txt file and open it so that
+        # its easier to edit and it can be accessed without running
+        # the program every time
+        path = os.path.dirname(__file__) + "\help.txt"
+        with open(path, "r") as f:
+            text = f.read()
+
+        self.help_label.config(text=text, bg=self.blue, fg="white", justify="left")
+
+
+
+
+
+
 
 if __name__ == "__main__":
     app = MyWindow()
